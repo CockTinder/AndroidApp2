3ca85328bea604f74da21e263e0bfb9436693eeb
aG0TH3 <anton.gothe@gmail.com>
2017-06-17 13:38:19 +0200

    added toTinder-Button in MainActivity

--- app/src/main/java/one/mixme/mixone/MainActivity.java
+++ app/src/main/java/one/mixme/mixone/MainActivity.java
@@ -1,8 +1,10 @@
 package one.mixme.mixone;
 
+import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
 
         final ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
         viewPager.setAdapter(new ChildItemPagerAdapter(viewPager));
+    }
+
+    public void switchTinder (View view){
+        Intent intent = new Intent(MainActivity.this, Tinder.class);
+        startActivity(intent);
     }
 }
