28d362226e9e736143d6aecf00c392f2be029d3e
aG0TH3 <anton.gothe@gmail.com>
2017-06-17 13:08:37 +0200

    added Slider in MainActivity

---
+++ app/src/main/java/one/mixme/mixone/MainActivity.java
@@ -0,0 +1,17 @@
+package one.mixme.mixone;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setAdapter(new ChildItemPagerAdapter(viewPager));
+    }
+}
